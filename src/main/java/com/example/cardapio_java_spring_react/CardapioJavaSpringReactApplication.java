9775aaeaa3959c44c74919e04a4099bbb5da4ef2
imarcaos <marcaos.melo@gmail.com>
2025-08-22 16:16:49 +0100

    Inicio projeto com spring initializr

---
+++ src/main/java/com/example/cardapio_java_spring_react/CardapioJavaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapio_java_spring_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioJavaSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioJavaSpringReactApplication.class, args);
+	}
+
+}
